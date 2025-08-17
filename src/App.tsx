2b970b71cf431d7fa57d49b54595e0b6ef872a8d
Himanshu-1903 <himanshuvitthalani7@gmail.com>
2025-08-17 17:28:17 +0530

    Initial commit : Building DigiMess using React TypeScript &  Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Building DigiMess
+      </h1>
+    </div>
+  )
+}
+
+export default App
